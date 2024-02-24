828c59b5d19b3f1e50c36773cd5f660c1dba3a95
allfadhil1 <146059293+allfadhil1@users.noreply.github.com>
2024-02-24 20:41:22 +0700

    Add files via upload

--- learnmore.php
+++ learnmore.php
@@ -9,11 +9,13 @@
     
 
 <body>
+
 <nav>
         <ul>
             <li><a class="active" href="Project.php">Home</a></li>
             <li><a href="ProjectProfil.php">Profil</a></li>
             <li class="dropdown">
+            
                 <a href="#medsos">Medsos</a>
                 <div class="dropdown-content">
                     <a href="https">Instagram</a>
@@ -38,19 +40,31 @@
     <div class="slider">
         <div class="slide" for="slide1" style="--img:url('ASSET/wp3258768.jpg')">
           <div class="content">
-<h2>Pesona Ikan</h2>
-<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis quidem, nesciunt saepe nam nemo sint cupiditate alias esse nihil illo beatae dolor nisi delectus aperiam laudantium corporis officia tempora.</p>
+              <h2>Pesona Ikan</h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis quidem, nesciunt saepe nam nemo sint cupiditate alias esse nihil illo beatae dolor nisi delectus aperiam laudantium corporis officia tempora.</p>
+              <br>
+               
+               
+               <a href="PesonaIkan.php"><button class="button button2">Blue</button></a>
+               
+              
           </div>
-        </div>
-        <div class="slide" for="slide2" style="--img:url('ASSET/f9f06d34b883685b4b0a19a64b493cba.jpg')">
-        <div class="content">
-<h2>Pesona Terumbu Karang</h2>
+         </div>
+          <div class="slide" for="slide2" style="--img:url('ASSET/f9f06d34b883685b4b0a19a64b493cba.jpg')">
+           <div class="content">
+            <h2>Pesona Terumbu Karang</h2>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis quidem, nesciunt saepe nam nemo sint cupiditate alias esse nihil illo beatae dolor nisi delectus aperiam laudantium corporis officia tempora.</p>
+<div class='tombol'>
+
+<a href="PesonaTerumbuKarang.php" class=btn btn3>Learn More</a>
+<button class="button button2">Blue</button>
+</div>
           </div></div>
         <div class="slide" for="slide3" style="--img:url('ASSET/78ac1ebbf683f9fc75adf5b6d7083c43.jpg')">
         <div class="content">
 <h2>Pesona Pantai</h2>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis quidem, nesciunt saepe nam nemo sint cupiditate alias esse nihil illo beatae dolor nisi delectus aperiam laudantium corporis officia tempora.</p>
+<button class=btn btn3>Learn More</button>
           </div></div>
     </div>
    </div>
